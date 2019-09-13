7c48385c3f6611a13aa3bdd24a026fa5970ed494
junaidtariq <junaidtariq166@gmail.com>
2019-09-13 13:20:31 +0500

    checkpoint has been changed to wikipedia checkpoint

--- deep_punc/tasks.py
+++ deep_punc/tasks.py
@@ -10,9 +10,9 @@ from deepsegment import DeepSegment
 def processing(rough_paragraph):
     segmenter = DeepSegment('en')
     list_of_sentences = segmenter.segment(rough_paragraph)
-    # corrector = DeepCorrect('deeppunct_params_en', 'deeppunct_checkpoint_wikipedia')
+    corrector = DeepCorrect('deeppunct_params_en', 'deeppunct_checkpoint_wikipedia')
     # corrector = DeepCorrect('deeppunct_params_en', 'deeppunct_checkpoint_google_news')
-    corrector = DeepCorrect('deep_punc/deeppunct_params_en', 'deep_punc/deeppunct_checkpoint_tatoeba_cornell')
+    # corrector = DeepCorrect('deep_punc/deeppunct_params_en', 'deep_punc/deeppunct_checkpoint_tatoeba_cornell')
     paragraph = ''
     for i in range(len(list_of_sentences)):
         sentence = corrector.correct(list_of_sentences[i])
